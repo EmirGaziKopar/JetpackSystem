63d625f9525686f9f8bf62e18b0baee228c63e0a
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-07-15 00:11:42 +0300

    1.2

--- JetpackSystem/Assets/CharacterJetpackSystem.cs
+++ JetpackSystem/Assets/CharacterJetpackSystem.cs
@@ -7,8 +7,10 @@ public class CharacterJetpackSystem : MonoBehaviour
     new Rigidbody2D rigidbody2D;
     [SerializeField] float newton=1f;
     new Transform transform;
-   /* [SerializeField] float rotationSpeed=1f;*/
+    [SerializeField] float rotationSpeed=1f;
+    [SerializeField] float horizontalSpeed = 1f;
 
+    
    
 
 
@@ -18,31 +20,54 @@ public class CharacterJetpackSystem : MonoBehaviour
         transform = GetComponent<Transform>();
     }
 
+    private void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+
+            transform.Rotate(0, 0, -rotationSpeed);
+
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+
+
+            transform.Rotate(0, 0, rotationSpeed);
+
+        }
+        if(horizontal<0)
+        {
+            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
+            transform.position += new Vector3(horizontal * horizontalSpeed, 0, 0);
+        }
+        if (horizontal>0)
+        {
+            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+            transform.position += new Vector3(horizontal * horizontalSpeed, 0, 0);
+        }
+    }
+
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.tag == "Floor" || collision.tag == "MainCamera")
         if (Input.GetMouseButton(0))
         {
+
                 rigidbody2D.AddForce(transform.up * newton);
+
+
                 
-        }
 
-        if (Input.GetKey(KeyCode.A)) 
-        {
+            }
 
-            
-            transform.Rotate(0, 0, -1);
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-
-
-            transform.Rotate(0, 0, 1);
-
-        }
         
-
+        
 
     }
 }
